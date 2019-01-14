85858e9999c34e73df636eeae1d1d2bc02471baa
wyettmacdonald <35835228+wyettmacdonald@users.noreply.github.com>
2019-01-14 18:31:18 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  */
 
 // Include config file
-require_once "config.php";
+//require_once "config.php";
 
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
